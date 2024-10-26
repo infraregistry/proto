f6e313e72ad5156f2052d818bac29ce4d53aa2a4
Matthew Davis <matthew@matthewdavis.io>
2024-10-25 19:02:03 -0500

    bump

--- gen/account/v1/accountv1connect/account.connect.go
+++ gen/account/v1/accountv1connect/account.connect.go
@@ -8,7 +8,7 @@ import (
 	connect "connectrpc.com/connect"
 	context "context"
 	errors "errors"
-	v1 "github.com/infraregistrt/proto/gen/account/v1"
+	v1 "github.com/infraregistrty/proto/gen/account/v1"
 	http "net/http"
 	strings "strings"
 )
---
+++ makefile
@@ -0,0 +1,3 @@
+build:
+	buf lint
+	buf generate
